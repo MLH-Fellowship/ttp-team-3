96ce7297fe16ea2bc63a54786f152705cb3e6309
John Stendardo <johnakasquib96@gmail.com>
2020-06-25 17:01:04 -0400

    added css for single book view

--- src/components/views/BookView.js
+++ src/components/views/BookView.js
@@ -1,29 +1,27 @@
 import React from "react";
+import "./styles/BookView.css";
 
 const BookView = (props) => {
   let searchBook;
   if (!props.book.volumeInfo) {
-    return <div>loading</div>
+    return <div>loading</div>;
+  } else {
+    searchBook = props.book;
+
+    if (!searchBook.volumeInfo.imageLinks) {
+      searchBook.imgUrl =
+        "https://via.placeholder.com/305x242?text=Placeholder";
+    } else {
+      searchBook.imgUrl = searchBook.volumeInfo.imageLinks.thumbnail;
+    }
   }
-  else{
-      searchBook=props.book
-     
-        if(!searchBook.volumeInfo.imageLinks){
-          searchBook.imgUrl="https://via.placeholder.com/305x242?text=Placeholder"
-        }
-        else{
-          searchBook.imgUrl=searchBook.volumeInfo.imageLinks.thumbnail
-        }
-       }
   return (
     <div className="book-container">
-      <img
-        src={searchBook.imgUrl}
-        width="200px"
-        alt={props.book.id}
-      />
-      <br />
-      {searchBook.volumeInfo.title}
+      <img src={searchBook.imgUrl} alt={props.book.id} />
+      <div id="title-and-desc">
+        <h1 id="title">{searchBook.volumeInfo.title}</h1>
+        <p>{searchBook.volumeInfo.description}</p>
+      </div>
     </div>
   );
 };
