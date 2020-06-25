2ba04f40a831d9ee3752b6af12306ade6ff2e344
Omaru Jawara <omaru.jawara94@gmail.com>
2020-06-24 20:09:37 -0400

    Using hard coded parameters for the api call, book cards are rendered

--- src/components/containers/BookListContainer.js
+++ src/components/containers/BookListContainer.js
@@ -1,27 +1,41 @@
 import React, { Component } from "react";
 import { fetchBooksThunker } from "../../thunks";
 import { connect } from "react-redux";
-
-
+import { BookListView } from "../views/index";
 
 class BookListContainer extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      books: [],
+      search: "javascript",
+    };
   }
 
   componentDidMount() {
     this.props.fetchBooks();
   }
-  check(){
-    console.log(this.props.book)
+  check() {
+    console.log(this.props.book[0].volumeInfo.imageLinks.smallThumbnail);
+    this.setState({
+      ...this.state,
+      books: this.props.book,
+    });
   }
   render() {
-    return <button onClick={()=>this.check()}>BOOKS</button>;
+    return (
+      <React.Fragment>
+        <div className="container">
+          <BookListView books={this.state.books} />
+        </div>
+        <button onClick={() => this.check()}>BOOKS</button>
+      </React.Fragment>
+    );
   }
 }
 const mapState = (state) => {
-  console.log(state.books)
+  console.log(state.books);
+  console.log("***", state.book, "***");
   return { book: state.books };
 };
 const mapDispatch = (dispatch) => {
@@ -29,4 +43,4 @@ const mapDispatch = (dispatch) => {
     fetchBooks: () => dispatch(fetchBooksThunker()),
   };
 };
-export default connect (mapState,mapDispatch)(BookListContainer);
+export default connect(mapState, mapDispatch)(BookListContainer);
--- src/components/views/BookListView.js
+++ src/components/views/BookListView.js
@@ -1,5 +1,28 @@
 import React from "react";
 
-export default function BookListView() {
-  return <div></div>;
+const BookCard = (props) => {
+  return (
+    <div className="card col-3">
+      <img src={props.image} alt="xxx" />
+      <h2>Title: {props.title}</h2>
+      <h4>Author: {props.author}</h4>
+    </div>
+  );
+};
+
+export default function BookListView(props) {
+  return (
+    <React.Fragment>
+      <div className="card-container row">
+        {props.books.map((info) => (
+          <BookCard
+            key={info.id}
+            image={info.volumeInfo.imageLinks.thumbnail}
+            title={info.volumeInfo.title}
+            author={info.volumeInfo.authors}
+          />
+        ))}
+      </div>
+    </React.Fragment>
+  );
 }
