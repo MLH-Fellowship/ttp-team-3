53ae5229fb9cd6801106affa4bdb68287da5fbd7
Yimni Chan <yimni.chan.yn@gmail.com>
2020-06-24 22:16:26 -0400

    add fetchNewestTenThunk feature

--- src/components/containers/HomePageContainer.js
+++ src/components/containers/HomePageContainer.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-
-
+import PropTypes from "prop-types";
+import { fetchNewestTenThunk } from "../../thunks";
 import { HomePageView } from "../views";
 
 class HomePageContainer extends Component {
+  componentDidMount() {
+    this.props.fetchNewestTen();
+  }
+
   render() {
     return <HomePageView 
     user={this.props.user}
+    newestTen={this.props.newestTen}
     />;
   }
 }
@@ -15,6 +20,18 @@ const mapState = (state) => {
 return {
   user: state.user,
 };
+};
+
+// Map dispatch to props;
+const mapDispatch = (dispatch) => {
+  return {
+    fetchNewestTen: () => dispatch(fetchNewestTenThunk()),
+  };
+};
+
+// Type check props;
+HomePageContainer.propTypes = {
+  newestTen: PropTypes.array.isRequired,
 };
 
 export default connect(mapState)(HomePageContainer);
--- src/components/views/HomePageView.js
+++ src/components/views/HomePageView.js
@@ -1,13 +1,35 @@
 import React from "react";
-
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 //basic Home page
 const HomePageView =(props)=> {
+  // if (!props.newestTen.length) {
+  //   return <div className="NewTen">There are no new book.</div>;
+  // }
+
     return (
       <div>
-HomePageView
+      <h2>Welcome to Book Store</h2>
+      <p> image</p>
+     
+      <p> Most recent Book</p>
+          <div>
+          {props.newestTen.map((new10) => (
+            <div key={new10.id}>
+              <Link to={`/campuses/${new10.id}`}>
+                <h3>{new10.volumeInfo.title}</h3>
+                <img src={new10.volumeInfo.imageLinks.thumbnail} width="200px" alt={new10.volumeInfo.title} />
+              </Link>
+            </div>
+          ))}
+          </div>
+
       </div>
     );
 }
+
+HomePageView.propTypes = {
+  newestTen: PropTypes.array.isRequired,
+};
 
 export default HomePageView;
