ba950306cf012929feede6a4309d08ebbe1b130e
Veggier <jiangzhongcai123@gmail.com>
2020-06-25 11:16:20 -0400

    shows the book

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,4 +2,4 @@
 // The aliases of reducers in this file will be assigned as the names of the keys in the Redux store, with the values being the respective individual reducers;
 export { default as allUsers}from "../store/utilities/allUsers";
 export { default as user}from "../store/utilities/user";
-export { default as books}from "../store/utilities/book";
+export { default as books}from "../store/utilities/allBooks";
